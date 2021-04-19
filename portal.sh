530002e0230ce73014755ca55e1417ee3e92e887
Hezsel <vhesselp@gmail.com>
2021-04-19 14:45:39 -0300

    hessel

--- portal.sh
+++ portal.sh
@@ -1,24 +1,25 @@
 echo "CHECKOUT MASTER - CORE.APP"
 cd core.app
-git checkout master
-echo "NPM INSTALL - CORE.APP"
-npm install
+git checkout portal-refactor
+git pull
 echo -e
 
 echo "CHECKOUT MASTER - CORE.API"
 cd ../core.api
-git checkout master
+git checkout portal-refactor
+git pull
 echo -e
 
 
 echo "CHECKOUT MASTER - PORTAL.APP"
 cd ../portal.app
 git checkout master-refactor
-echo "NPM INSTALL - PORTAL.APP"
-npm install
+git pull
 echo -e 
 
 echo "CHECKOUT MASTER - PORTAL.API"
 cd ../portal.api
-git checkout master
+git checkout portal-refactor
+git pull
 echo -e
+
